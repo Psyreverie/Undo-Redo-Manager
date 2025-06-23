24d176e78c11c493497bcb2e722e5e2b76025302
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-06-23 01:11:54 -0230

    Finished the main assignment

---
+++ src/UndoRedoManager.java
@@ -0,0 +1,48 @@
+class Node<T> {
+    T state;
+    Node<T> prev;
+    Node<T> next;
+
+    Node(T state) {
+        this.state = state;
+    }
+}
+
+public class UndoRedoManager<T> {
+    private Node<T> current;
+
+    public void addState(T newState) {
+        Node<T> newNode = new Node<>(newState);
+        if (current != null) {
+            // Remove forward history
+            current.next = null;
+            newNode.prev = current;
+            current.next = newNode;
+        }
+        current = newNode;
+    }
+
+    public boolean undo() {
+        if (current != null && current.prev != null) {
+            current = current.prev;
+            return true;
+        }
+        System.out.println("No more undo steps.");
+        return false;
+    }
+
+    public boolean redo() {
+        if (current != null && current.next != null) {
+            current = current.next;
+            return true;
+        }
+        System.out.println("No more redo steps.");
+        return false;
+    }
+
+    public T getCurrentState() {
+        return current != null ? current.state : null;
+    }
+
+    }
+
